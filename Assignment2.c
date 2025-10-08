6f5c0241d88c6b43840554012b8a49e8797b5bf1
major-pro1 <angelmakola35@gmail.com>
2025-10-08 12:16:58 +0200

    initial commit

---
+++ Assignment2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    //i will be using dowhile loop to print out hello world five times
+    int i =0;
+    do{
+        printf("Hello, World!\n");
+        i++;
+    } while(i <5);
+
+    return 0;
+}
